2a9c118e8ec16fc3ce690f2cbf0644dd0b71615a
rus100 <rus100@yandex.ru>
2016-11-09 00:42:56 +0700

    Create Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Camera
+{
+   static class Settings
+            {
+      public static int height = 300;
+      public static int width = 300;
+    }
+}
